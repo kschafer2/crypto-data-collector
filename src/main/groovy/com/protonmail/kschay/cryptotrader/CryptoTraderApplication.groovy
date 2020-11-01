2717cd6438860bd021719ac89b5df148f4bca782
Kyle Schafer <kschay@protonmail.com>
2020-10-31 19:01:17 -0500

    Project initialization

---
+++ src/main/groovy/com/protonmail/kschay/cryptotrader/CryptoTraderApplication.groovy
@@ -0,0 +1,13 @@
+package com.protonmail.kschay.cryptotrader
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CryptoTraderApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CryptoTraderApplication, args)
+	}
+
+}
