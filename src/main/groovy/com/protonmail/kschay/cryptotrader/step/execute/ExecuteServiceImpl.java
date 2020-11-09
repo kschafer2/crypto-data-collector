43a31baa75fa8a7a67d7e7ef70c0fb317ca71f83
Kyle Schafer <kschay@protonmail.com>
2020-11-08 19:18:23 -0600

    Removed unused code

--- src/main/groovy/com/protonmail/kschay/cryptotrader/step/execute/ExecuteServiceImpl.java
+++ src/main/groovy/com/protonmail/kschay/cryptotrader/step/execute/ExecuteServiceImpl.java
@@ -7,11 +7,7 @@ import com.protonmail.kschay.cryptotrader.domain.fee.Fee;
 import com.protonmail.kschay.cryptotrader.domain.fee.FeeClient;
 import com.protonmail.kschay.cryptotrader.domain.symbol.Symbol;
 import com.protonmail.kschay.cryptotrader.domain.ticker.SimpleTickerClient;
-import com.protonmail.kschay.cryptotrader.domain.trade.Trade;
-import com.protonmail.kschay.cryptotrader.domain.trade.TradeClient;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 import static com.protonmail.kschay.cryptotrader.domain.side.Side.BUY;
 import static com.protonmail.kschay.cryptotrader.domain.side.Side.SELL;
@@ -23,16 +19,13 @@ public class ExecuteServiceImpl implements ExecuteService {
     private final SimpleTickerClient simpleTickerClient;
     private final Fee fee;
     private final BalanceClient balanceClient;
-    private final TradeClient tradeClient;
 
     public ExecuteServiceImpl(SimpleTickerClient simpleTickerClient,
                               FeeClient feeClient,
-                              BalanceClient balanceClient,
-                              TradeClient tradeClient) {
+                              BalanceClient balanceClient) {
         this.simpleTickerClient = simpleTickerClient;
         this.fee = feeClient.getFee();
         this.balanceClient = balanceClient;
-        this.tradeClient = tradeClient;
     }
 
     @Override
@@ -47,18 +40,6 @@ public class ExecuteServiceImpl implements ExecuteService {
         else if(executeOrder.getSide().equals(SELL)) {
             executeOrder.setPrice(simpleTickerClient.getSimpleTicker(symbol).getBid());
             executeOrder.setAmount(balanceClient.getCurrentBalance(symbol.base()).getAvailable());
-        }
-        return executeOrder;
-    }
-
-    private ExecuteOrder subtractExecutedTrades(ExecuteOrder executeOrder) {
-        tradeClient.getTradesSinceClose(executeOrder.getSymbol())
-                .stream()
-                .map(Trade::getAmount)
-                .forEach(executeOrder::subtractFromAmount);
-
-        if(executeOrder.getAmount().equals(BigDecimal.ZERO.toString())) {
-            return null;
         }
         return executeOrder;
     }
